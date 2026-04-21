e17cc4699bcbe3ec9f66c0758e103caaf3325f9c
Joana Serralheiro <0102523@pt.softinsa.com>
2026-04-21 10:56:11 +0100

    fix duplicated

---
+++ clean_db.py
@@ -0,0 +1,29 @@
+import urllib.request
+import json
+import urllib.parse
+api_key = '$2a$10$b1zk.UlxUmhefLmBtQ66DOhVPYpHQN6tYqtixNMQrAJGBDCWlYxey'
+
+for bin_id in ['69d3d50daaba882197cd3fbd', '69e69213aaba8821971d5604']:
+    req_get = urllib.request.Request(f'https://api.jsonbin.io/v3/b/{bin_id}/latest', headers={'X-Master-Key': api_key, 'User-Agent': 'Mozilla/5.0'})
+    resp = urllib.request.urlopen(req_get)
+    data = json.loads(resp.read().decode('utf-8'))['record']
+
+    duplicate_count = 0
+    if 'sprints' in data:
+        for s in data['sprints']:
+            if 'items' in s:
+                unique_items = {}
+                for item in reversed(s['items']): # keep latest by looping reverse and not overwriting
+                    if item['name'] not in unique_items:
+                        unique_items[item['name']] = item
+                    else:
+                        duplicate_count += 1
+                s['items'] = list(reversed(list(unique_items.values()))) # restore original order
+    
+    if duplicate_count > 0:
+        encoded = json.dumps(data, ensure_ascii=False).encode('utf-8')
+        req_put = urllib.request.Request(f'https://api.jsonbin.io/v3/b/{bin_id}', data=encoded, method='PUT', headers={'X-Master-Key': api_key, 'Content-Type': 'application/json; charset=utf-8', 'User-Agent': 'Mozilla/5.0'})
+        urllib.request.urlopen(req_put)
+        print(f"Removed {duplicate_count} duplicates from bin {bin_id}")
+    else:
+        print(f"No duplicates found in bin {bin_id}")
